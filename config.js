695a1986c47985207a9bb26012dd46873ac74f8a
Rami Taleb <rtale089@uottawa.ca>
2018-01-15 10:33:10 -0500

    config file update

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	consumer_key: 			'...',
-	consumer_secret: 		'...',
-	access_token: 			'...',
-	access_token_secret: 	'...'
+	consumer_key: 			'gusWMgPFvRdcmCburzq1nlCMN',
+	consumer_secret: 		'r7R2mLTWJsk3F2umGpfJJFelBqfYo3f6ioh66D93PPZuPRwFp4',
+	access_token: 			'949408409547243520-tXS4POVZFQGdu9OiNy2jMECHM7VQ9tk',
+	access_token_secret: 	'k5zwn2YwwfM01y1z7Jjptk3qjF6GTI7JuyTLnevKUMJN8'
 }
